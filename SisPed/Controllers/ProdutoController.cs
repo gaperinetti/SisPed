6d445d11c08c62352419d63a8cd00d34f1a0565c
Gabriel Perinetti <gaperinetti@gmail.com>
2017-05-30 13:54:20 -0300

    Tratamento campos nulo id e produto

--- SisPed/Controllers/ProdutoController.cs
+++ SisPed/Controllers/ProdutoController.cs
@@ -3,6 +3,7 @@ using SisPed.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -22,7 +23,17 @@ namespace SisPed.Controllers
         // GET: Produto/Details/5
         public ActionResult Details(int id)
         {
+            if(id == null) // tratamento campo nulo para nao travar o sistema
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
             var produto = db.Produto.Find(id);//busca id do produto para mostrar detalhes
+
+            if(produto == null) // tratamento campo nulo para nao travar o sistema
+            {
+                return HttpNotFound();
+            }
+
             return View(produto); // chama a view detalhes com o produto buscado do id
         }
 
